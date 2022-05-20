42ca484e037c3ce28c1c9fb3165ca7c3bdea3d46
SemihKÖKÇÜ <semihkkc1@gmail.com>
2022-05-20 14:32:29 +0300

    Startup Configure

--- MovieApp.Web/Startup.cs
+++ MovieApp.Web/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MovieApp.Web.Data;
 using MovieApp.Web.Entity;
+using MovieApp.Web.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,9 +28,47 @@ namespace MovieApp.Web
             services.AddMvc();
             services.AddDbContext<MovieContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("MsSQLConnection")));
-            services.ConfigureApplicationCookie(opt => opt.LoginPath = "Account/Login");
-            
+            //services.ConfigureApplicationCookie(opt => opt.LoginPath = "Account/Login");
 
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options => {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+
+                options.SignIn.RequireConfirmedEmail = true;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
+            services.ConfigureApplicationCookie(options => {
+
+                options.LoginPath = "Account/Login";
+                options.LogoutPath = "Account/Logout";
+                options.AccessDeniedPath = "Account/AccessDenied";
+                options.SlidingExpiration = true; // sistem yeniden girme kapalı
+                options.Cookie = new CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name = ".AspNetCoreDemo.Access.Cookie",
+                    Path = "/",
+                    SameSite = SameSiteMode.Lax,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest
+                };
+
+                    
+
+            });
 
             services.AddControllersWithViews()
                 .AddViewOptions(options => options.HtmlHelperOptions.ClientValidationEnabled=true);
